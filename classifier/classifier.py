1f9705a4fa7b4ffcb50ce50428588b7425ff43c4
julianVelandia <jvelandiag@unal.edu.co>
2022-10-02 23:35:44 -0500

    Detección mediate video e imagen

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -26,9 +26,23 @@ def moving_object_detection_video():
 def moving_object_detection_frame():
     model = torch.hub.load('ultralytics/yolov5', 'custom',
                            path='classifier/model/best.pt')
+    while True:
+        img_name = input('\nDigita [P] para una imagen personalizada \n'
+                     'Digita [D] para una imagen por defecto \n')
+
+        if img_name == 'p' or img_name == 'P':
+            try:
+                frame = cv2.imread('classifier/' + img_name)
+            except Exception as e:
+                print('La imágen no se encuentra,\n'
+                      ' - Recuerde mover la imágen a la carpeta classifier\n'
+                      ' - Recuerde digitar con la extención .PNG o .JPG\n')
+            break
+        elif img_name == 'd' or img_name == 'D':
+            frame = cv2.imread('classifier/DJI_0370.JPG')
+            break
 
 
-    frame = cv2.imread('classifier/DJI_0370.JPG')
     detect = model(frame)
 
     info = detect.pandas().xyxy[0]
